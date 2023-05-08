29418a9aa542257e7c4d3b1313fdec56e4ffd2b9
Warre <warre.dujardin@gmail.com>
2023-05-08 02:02:00 +0200

    fix app rs

--- src/app.rs
+++ src/app.rs
@@ -33,7 +33,6 @@ pub fn App(cx: Scope) -> impl IntoView {
 fn HomePage(cx: Scope) -> impl IntoView {
     // Creates a reactive value to update the button
     let (count, set_count) = create_signal(cx, 0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! { cx,
         <main class="my-0 mx-auto max-w-3xl text-center">
